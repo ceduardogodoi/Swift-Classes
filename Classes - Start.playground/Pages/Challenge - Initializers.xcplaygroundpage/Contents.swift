b3adb4299c60ce2430bb84d4e4c46e2905523cf0
ceduardogodoi <carloseduardoalvesgodoi@hotmail.com>
2019-06-22 14:21:18 -0300

    Challenge: Initializers and Protocols

--- Classes - Start.playground/Pages/Challenge - Initializers.xcplaygroundpage/Contents.swift
+++ Classes - Start.playground/Pages/Challenge - Initializers.xcplaygroundpage/Contents.swift
@@ -36,7 +36,14 @@
  2. a required initializer that takes `name` as a parameter
  3.  a function `speak()` that does nothing.
  */
+class Animal {
+    let name: String
+    required init(name: String) {
+        self.name = name
+    }
 
+    func speak() { }
+}
 /*:
  Create a class named `Dog` that…
  1. inherits from `Animal`
@@ -44,11 +51,37 @@
  3. implements the required initializer, defaulting the trick count to `0`, and calling `speak()` at the end
  4. overrides the function `speak()` to greet you and says its name
  */
+class Dog: Animal {
+    var tricksLearnedCount: Int
+
+    convenience required init(name: String) {
+        self.init(name: name, tricksLearnedCount: 0)
+    }
+
+    init(name: String, tricksLearnedCount: Int) {
+        self.tricksLearnedCount = tricksLearnedCount
+        super.init(name: name)
+        speak()
+    }
+
+    override func speak() {
+        print("Bow wow! My name is \(name)!")
+    }
+}
+
+Dog(name: "Shadow")
 
 /*:
  Add a second (non-required) initializer to `Dog` that takes both the `name` and `numTricksLearned` as parameters. Then call this initializer from the required initializer.
  */
-
+Dog(name: "Chance", tricksLearnedCount: 3)
 /*:
  In an extension, add a convenience initializer to `Dog` that defaults the dog's name to your favorite dog's name, with however many tricks the dog has learned.
  */
+extension Dog {
+    convenience init(tricksLearnedCount: Int = .max) {
+        self.init(name: "Tramp", tricksLearnedCount: tricksLearnedCount)
+    }
+}
+
+Dog().tricksLearnedCount
--- Classes - Start.playground/Pages/Protocols.xcplaygroundpage/Contents.swift
+++ Classes - Start.playground/Pages/Protocols.xcplaygroundpage/Contents.swift
@@ -1,37 +1,58 @@
-class Animal {
-  let name: String
-  
-  required init(name: String) {
-    self.name = name
-  }
-  
-  func speak() { }
+protocol Aloof {
+    var name: String { get }
+}
+
+extension Aloof {
+    var greeting: String {
+        return "My name is \(name). Please leave me alone."
+    }
+}
+
+protocol Animal {
+    var name: String { get }
+
+    init(name: String)
+
+    func speak()
+}
+
+protocol AloofAnimal: Aloof, Animal { }
+
+extension AloofAnimal {
+    func speak() {
+        print("\(greeting). I must look at this wall.")
+    }
 }
 
 class Dog: Animal {
-  var tricksLearnedCount: Int
-  
-  init(name: String, tricksLearnedCount: Int) {
-    self.tricksLearnedCount = tricksLearnedCount
-    super.init(name: name)
-  }
-  
-  convenience required init(name: String) {
-    self.init(name: name, tricksLearnedCount: 0)
-  }
-  
-  override func speak() {
-    print("Bow wow! My name is \(name)!")
-  }
+    let name: String
+    var tricksLearnedCount: Int
+    
+    init(name: String, tricksLearnedCount: Int) {
+        self.tricksLearnedCount = tricksLearnedCount
+        self.name = name
+    }
+    
+    convenience required init(name: String) {
+        self.init(name: name, tricksLearnedCount: 0)
+    }
+    
+    func speak() {
+        print("Bow wow! My name is \(name)!")
+    }
 }
 
-class Cat: Animal {
-  override func speak() {
-    print("My name is \(name). Please leave me alone. I must look at this wall.")
-  }
+class Cat {
+    let name: String
+
+    required init(name: String) {
+        self.name = name
+    }
 }
 
-let animals = [Dog(name: "Fang"), Cat(name: "Mr. Midnight")]
+extension Cat: AloofAnimal { }
+
+let animals: [Animal] = [Dog(name: "Fang"), Cat(name: "Mr. Midnight")]
 for animal in animals {
-  animal.speak()
+    animal.speak()
 }

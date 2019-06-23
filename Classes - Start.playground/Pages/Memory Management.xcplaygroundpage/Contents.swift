80f2de68b336a602f8a67e779ef62d791f14c230
ceduardogodoi <carloseduardoalvesgodoi@hotmail.com>
2019-06-23 20:47:08 -0300

    Memory Management

--- Classes - Start.playground/Pages/Memory Management.xcplaygroundpage/Contents.swift
+++ Classes - Start.playground/Pages/Memory Management.xcplaygroundpage/Contents.swift
@@ -1,7 +1,20 @@
 class Person {
-  let name: String
-  
-  init(name: String) {
-    self.name = name
-  }
+    let name: String
+    weak var bestBuddy: Person?
+    
+    init(name: String) {
+        self.name = name
+    }
+
+    deinit {
+        print("The memory for \(name) has been deallocated!")
+    }
 }
+
+var catty: Person? = Person(name: "Catty Pantherwaul")
+var loki: Person? = Person(name: "Loki Moakley")
+loki?.bestBuddy = catty
+catty?.bestBuddy = loki
+
+loki = nil
+catty = nil
